33ee374f5ccbddc55a7e7885a7bbce3afd57c267
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-19 11:11:29 -0500

    Feat: Add migrations examples

---
+++ EntityFrameworkCore/DataAccess/Migrations/20221019153135_AddDisplayOrderToGenreTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AddDisplayOrderToGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Genre",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Genre");
+        }
+    }
+}
---
+++ EntityFrameworkCore/DataAccess/Migrations/20221019154245_RemoveDisplayOrderColumnFromGenreTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class RemoveDisplayOrderColumnFromGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Genre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Genre",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
