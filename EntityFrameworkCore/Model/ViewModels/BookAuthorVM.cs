d17ba512abb30af52adfb87ffbc61fea671b4cab
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-20 18:32:19 -0500

    Feat: Add examples to manage ManyToMany relatinships using EF

---
+++ EntityFrameworkCore/Model/ViewModels/BookAuthorVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModels
+{
+    public class BookAuthorVM
+    {
+        public Fluent_BookAuthor BookAuthor { get; set; }
+        public Fluent_Book Book { get; set; }
+        public IEnumerable<Fluent_BookAuthor> BookAuthorList { get; set; }
+        public IEnumerable<SelectListItem>  AuthorList { get; set; }
+    }
+}
